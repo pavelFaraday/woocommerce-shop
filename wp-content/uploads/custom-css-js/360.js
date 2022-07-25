4023e65fc358eabfcc78d83a16482c5d516bcbec
pavelFaraday <devstudio.corp@gmail.com>
2022-07-25 20:58:04 +0400

    Customise Search Functionality (45-47)

---
+++ wp-content/uploads/custom-css-js/360.js
@@ -0,0 +1,6 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+document.getElementsByClassName('search-form')[0].innerHTML += '<input type="hidden" name="post_type" value="product">';</script>
+<!-- end Simple Custom CSS and JS -->
